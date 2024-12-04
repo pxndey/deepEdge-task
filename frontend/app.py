404c47ac1ead9a9685a43e527870ec23a51292db
Anushk Pandey <anushkpandey@gmail.com>
2024-12-05 01:05:57 +0530

    feat: frontend

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import requests
 
+api_url = "http://localhost:5001/query"
+
 st.title("LLM-based RAG Search")
 
 # Input for user query
@@ -9,14 +11,16 @@ query = st.text_input("Enter your query:")
 
 if st.button("Search"):
     # Make a POST request to the Flask API
-    print("accessing ", "<Flask app string>", " with query ", query)
-    response = None # call the flask app and get response
-
+    payload = {"query": query}
+    response = requests.post(api_url, json=payload)
+    print("accessing ", f"{api_url}", " with query ", query)
+    
     # implement the flask call here
     
     if response.status_code == 200:
         # Display the generated answer
-        answer = response.json().get('answer', "No answer received.")
-        st.write("Answer:", answer)
+        st.markdown(response.text)
+        # answer = response.json().get('answer', "No answer received.")
+        # st.write("Answer:", answer)
     else:
         st.error(f"Error: {response.status_code}")
